9c15f5c713c057751e1f26709fc854328f35641b
Schuck <shooterman@users.noreply.github.com>
2014-10-29 21:36:45 +0800

    Update install_ruby.sh

--- sh/install_ruby.sh
+++ sh/install_ruby.sh
@@ -1,4 +1,4 @@
-```
+
 sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
 
@@ -15,4 +15,3 @@ exec $SHELL
 rbenv install 2.1.3
 rbenv global 2.1.3
 ruby -v
-```
