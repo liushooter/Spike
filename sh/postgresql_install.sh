8960bce5bcb72d244b4b2ecb631417bafe2fb6f7
shooter <shooter.hero@gmail.com>
2014-10-30 16:53:11 +0800

    up

--- sh/postgresql_install.sh
+++ sh/postgresql_install.sh
@@ -1,3 +1,13 @@
 sudo apt-get update
 
 sudo apt-get install libpq-dev postgresql
+
+# sudo -u postgres createuser --superuser <username> #创建用户
+
+# production:
+#   adapter: postgresql
+#   encoding: utf-8
+#   database: <database>
+#   username: <username>
+#   password: ''
+#   pool: 5
