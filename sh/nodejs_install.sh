cac3268010a2ea3c8b022cf7fd52724c19986750
Shooter <shooter@bitcoincentury.org>
2018-04-06 21:19:01 +0800

    Update nodejs_install.sh
    zqw

---
+++ sh/nodejs_install.sh
@@ -0,0 +1,10 @@
+# Using NVM
+
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.profile
+nvm install v8.10.0
+nvm alias default 8.10.0
+nvm use default
+
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
