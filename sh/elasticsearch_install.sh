2af0c96cb39c11a9bb2e352029d19056810a008b
Shooter <shooter@bitcoincentury.org>
2018-04-12 18:06:47 +0800

    Update elasticsearch_install.sh

--- sh/elasticsearch_install.sh
+++ sh/elasticsearch_install.sh
@@ -1,12 +1,12 @@
-sudo apt-get -qq update && sudo apt-get install -qqy openjdk-7-jre-headless
 
-wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo add-apt-repository -y ppa:webupd8team/java
 
-sudo sh -c "echo 'deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main' >> /etc/apt/sources.list"
+sudo apt-get update
 
-sudo apt-get update && sudo apt-get install -y elasticsearch
+sudo apt-get -y install oracle-java8-installer
 
-sudo service elasticsearch start
+java -version
 
-# http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/setup-repositories.html
-# http://stackoverflow.com/questions/1584066/append-to-etc-apt-sources-list
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.3.deb
+
+sudo dpkg -i elasticsearch-6.2.3.deb
